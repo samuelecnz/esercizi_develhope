61b346021e5435795e227f797e49950eaa1c4a8c
Samuele Canzoneri <samuele.cnz@gmail.com>
2022-12-15 15:41:34 +0100

    if-statement3

---
+++ if-statement-3/src/it/develhope/ifstatement/Start.java
@@ -0,0 +1,11 @@
+package it.develhope.ifstatement;
+
+public class Start {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
---
+++ if-statement-3/src/it/develhope/ifstatement/Student.java
@@ -0,0 +1,4 @@
+package it.develhope.ifstatement;
+
+
+
