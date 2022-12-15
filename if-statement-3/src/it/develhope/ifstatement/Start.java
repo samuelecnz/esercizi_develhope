34ee9c3c1afcff738f3b42b54504f16ce89a2940
Samuele Canzoneri <samuele.cnz@gmail.com>
2022-12-15 16:07:01 +0100

    if-statement-3

--- if-statement-3/src/it/develhope/ifstatement/Start.java
+++ if-statement-3/src/it/develhope/ifstatement/Start.java
@@ -3,9 +3,7 @@ package it.develhope.ifstatement;
 public class Start {
 
     public static void main(String[] args) {
-
-        
-
-    }
-
-}
+                Student student = new Student("Diego", 26);
+                student.guessingAge();
+            }
+        }
--- if-statement-3/src/it/develhope/ifstatement/Student.java
+++ if-statement-3/src/it/develhope/ifstatement/Student.java
@@ -1,4 +1,30 @@
 package it.develhope.ifstatement;
 
+import java.util.Random;
+
+    public class Student {
+        private String name;
+        private int age;
+
+        public Student(String studentName, int studentAge) {
+            name = studentName;
+            age = studentAge;
+        }
+
+        public void guessingAge() {
+            Random rand = new Random();
+            int randomValue = rand.nextInt(35);
+
+            System.out.println("Ho generato il numero random " + randomValue + " per lo studente " + name + ".");
+
+            if (randomValue == age) {
+                System.out.println("Il numero generato equivale all'eta' dello studente.");
+            } else if (randomValue > age) {
+                System.out.println("Il numero generato e' maggiore all'eta' dello studente.");
+            } else {
+                System.out.println("Il numero generato e' minore all'eta' dello studente.");
+            }
+        }
+    }
 
 
